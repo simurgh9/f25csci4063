de9e246ec59caf1e33bc35b94bef010f2bd0601b
Ryan Seaman <ryan.l.seaman@gmail.com>
2025-09-30 20:12:44 -0500

    added functionality to save transcript to db

---
+++ src/server/src/model/entities/chunk.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne, Unique } from "typeorm"
+import { Episode } from "./episode"
+
+@Entity()
+@Unique(["episode", "index"])
+export class Chunk extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    index!: number
+
+    @ManyToOne(() => Episode, (episode) => episode.chunks, { onDelete: "CASCADE" })
+    episode!: Episode
+
+    @Column()
+    text!: string
+
+    @Column("vector", { length: 1536})
+    embedding!: number[] | Buffer
+}
