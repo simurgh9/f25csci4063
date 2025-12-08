b9950029dfbdeb264f1f36054cc4ba1901029ac4
Ryan Seaman <ryan.l.seaman@gmail.com>
2025-12-08 10:01:12 -0600

    exposed controller logic via router

--- src/server/src/routes/UserRouter.ts
+++ src/server/src/routes/UserRouter.ts
@@ -7,7 +7,9 @@ const userRouter = Router();
 
 userRouter.post("/create", userController.create);
 userRouter.put("/shows", userController.addShowForUser);    
-userRouter.post("/subscriptionInfo", userController.addCurrentEpisode);
+userRouter.post("/subscriptionInfo", userController.getSubscriptionInfoForUser);
+userRouter.get("/subscriptionInfo", userController.addCurrentEpisode);
+userRouter.post("/unsubscribe", userController.unsubscribeFromShow);
 userRouter.get("/:id", userController.get);
 userRouter.delete("/:id", userController.delete);
 userRouter.get("/posts/:userId", userController.getPostsForUser);
